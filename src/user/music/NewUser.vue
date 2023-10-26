df7f95ecbdc21d712b065ec02ccc0dc2dd16a3a7
Agbooa12 <agboolaelijah077@gmail.com>
2023-10-26 22:11:32 +0100

    working on the user interface

--- src/user/music/NewUser.vue
+++ src/user/music/NewUser.vue
@@ -16,7 +16,14 @@
 </div>
         <div class="row">
           <div class="col-md-3 " v-for="(artist, index) in artists" :key="index" >
-            
+            <div class="card" style="width:400px">
+              <img class="card-img-top" src="img_avatar1.png" alt="Card image">
+              <div class="card-body">
+                <h4 class="card-title">John Doe</h4>
+                <p class="card-text">Some example text.</p>
+                <a href="#" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
       </div>
     </div>
 </template>
