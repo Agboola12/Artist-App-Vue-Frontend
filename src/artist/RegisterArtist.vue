a74b2a949f08989bbe4211f882a99f3327012163
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-03 11:19:20 +0100

    still working on the new feature to the design

--- src/artist/RegisterArtist.vue
+++ src/artist/RegisterArtist.vue
@@ -26,7 +26,19 @@
             />
           </div>
           <div class="form-group">
-            
+            <fieldset>
+                 <p>Please select your type of music:</p>
+                 <div>
+                   <input type="radio" id="contactChoice1" name="contact" value="email" />
+                   <label for="contactChoice1">Email</label>
+
+                   <input type="radio" id="contactChoice2" name="contact" value="phone" />
+                   <label for="contactChoice2">Phone</label>
+
+                   <input type="radio" id="contactChoice3" name="contact" value="mail" />
+                   <label for="contactChoice3">Mail</label>
+                 </div>
+            </fieldset>
           </div>
           <div class="form-group">
             <input
