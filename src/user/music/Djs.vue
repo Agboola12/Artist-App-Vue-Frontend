5b86a5d87f85cef106f53ff3e6bb8d80e4f21245
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-05 22:23:52 +0100

    working on  the new features

--- src/user/music/Djs.vue
+++ src/user/music/Djs.vue
@@ -2,14 +2,18 @@
       <UserSideBar/>
       <div class="main-content">
           <div class="container">
-              <div class="row" v-if="user">
-                    <div class="col-md-3">
-
-                    </div>
+              <div class="row" >
+                    <div class="col-md-3" v-if="user">
+                      <img class="card-img-top w-100" :src="user.imageUrl" alt="Card image" height="300" >
+              <div class="card-body">
+                <h4 class="card-title">{{ user.firstName }}</h4>
+                <p class="card-text">{{user.musicType}}</p>
               </div>
-              <div v-else>
-                  <p>Please log in to access your profile.</p>
-                </div>
+                    </div>
+                    <div v-else>
+                        <p>No artist available.</p>
+                      </div>
+              </div>
           </div>
           
       </div>
@@ -33,8 +37,8 @@ export default {
        getDjs(){
         axios.get("http://localhost:8000/getDjs")
           .then((res) => {
-            console.log(res.data);
-            // this.user = res.data.data;
+            console.log(res.data.data);
+            this.user = res.data.data;
           })
           .catch((error) => {
             console.error(error);
