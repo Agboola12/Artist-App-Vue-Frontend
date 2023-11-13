958ee4945dc8262393b7886429b3114c7678f13c
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-13 09:23:31 +0100

    working on frontend

--- src/Land.vue
+++ src/Land.vue
@@ -1,18 +1,42 @@
 <template>
         <div class="body p-5">
             <header>
+              <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Sridhar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse  navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
                 <nav class=" shadow navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <h4 class="navbar-brand" href="#" style="color: #A10035;">Prime</h4>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav ml-auto">
+                      <ul class="navbar-nav">
                         <li class="nav-item active">
                           <a class="nav-link" href="#">Home </a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
+                          <a class="nav-link" href="#">About</a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link disabled">Disabled</a>
@@ -33,7 +57,9 @@ export default {
 <style scoped>
 .body{
   font-family: Verdana, Geneva, Tahoma, sans-serif;
-  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)),url(../assets/download.jfif);
+  background: linear-gradient(#A10035, #FFDDDD);
+  /* background-color:linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)) #A10035; */
+  /* background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)),url(../assets/download.jfif); */
   background-size: cover;
   background-attachment: fixed;
   -moz-background-size: cover;
