7451888d68ba2064b703908c7ce315145b7a0725
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-13 09:45:16 +0100

    working on frontend

--- src/Land.vue
+++ src/Land.vue
@@ -1,52 +1,15 @@
 <template>
         <div class="body p-5">
-            <header>
-
-              <nav class="navbar navbar-expand-lg navbar-light bg-light p-2 " style="color: #001d47" >
-                    <h4 class="navbar-brand" href="#" style="color: #A10035;">Prime</h4>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  
-  <div class="collapse navbar-collapse" id="navbarNav">
-    
-    <ul class="navbar-nav  mx-auto text-center" id="navbar">
-        <li class="nav-item mx-auto">
-            <a class="nav-link  text-dark text-center"  href="#" routerLink="home">Home</a>
-        </li>
-     
-     <li class="nav-item">
-        <a class="nav-link text-dark"  href="#" routerLink="/user-home">Account</a>
-    </li>
-  
-        <!-- <li class="nav-item dropdown">
-            <a class="nav-link  text-dark"     >Account</a>
-        </li> -->
-  
-      <li class="nav-item">
-        <a class="nav-link text-dark"  href="#" routerLink="/contact">Contact</a>
-    </li>
-     <li class="nav-item">
-        <a class="nav-link text-dark"  href="#" routerLink="/findabranch">Find a branch</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link text-dark"  href="#" routerLink="/partner">Sponsorship</a>
-    </li>
-    <li class="nav-item">
-            <a class="nav-link  text-dark"  href="#" routerLink="/question">Question</a>
-        </li>
-    </ul>
-  </div>
-  </nav>
-                <nav class=" shadow navbar navbar-expand-lg navbar-light bg-light">
+            <header class="shadow">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light ">
                     <h4 class="navbar-brand" href="#" style="color: #A10035;">Prime</h4>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav">
+                      <ul class="navbar-nav" style="margin-left: 70%;">
                         <li class="nav-item active">
-                          <a class="nav-link" href="#">Home </a>
+                          <a class="nav-link " href="#">Home </a>
                         </li>
                         <li class="nav-item">
                           <a class="nav-link" href="#">About</a>
@@ -57,6 +20,16 @@
                       </ul>
                 </div>
                 </nav>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <h3>Vibe to the song</h3>
+                        <p>Enjoy the music.</p>
+                    </div>
+                    <div class="image col-md-6">
+
+                    </div>
+                </div>
               </header>
         </div>
 </template>
@@ -79,8 +52,21 @@ export default {
   -webkit-background-size: cover;
   -o-background-size: cover;
   overflow: hidden;
+  height: auto;
+  width: 100%;
+}
+
+header{
+  background: white;
+  background-size: cover;
+  background-attachment: fixed;
+  -moz-background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  overflow: hidden;
   height: 100vh;
   width: 100%;
-    
+  
+
 }
 </style>
