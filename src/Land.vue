3e77536a84282b8a29b251da7c6fe93cb1c9d812
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-13 11:30:04 +0100

    working on frontend

--- src/Land.vue
+++ src/Land.vue
@@ -1,33 +1,36 @@
 <template>
-        <div class="body p-5">
-            <header class="shadow ">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-                    <h4 class="navbar-brand" href="#" style="color: #A10035;">Prime</h4>
+        <div class="body">
+            <header class="shadow-sm" style="border-radius: 20px;" >
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <h4 class="navbar-brand" href="#" style="color: #FFDDDD; font-size: 2em;"><b>Prime</b></h4>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul class="navbar-nav" style="margin-left: 70%;">
                         <li class="nav-item active">
-                          <a class="nav-link " href="#">Home </a>
+                          <a class="nav-link  " href="#">Home </a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="#">About</a>
+                          <a class="nav-link " href="#">About</a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link disabled">Disabled</a>
+                          <a class="nav-link " href="register">User</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link " href="registerartist">Artist</a>
                         </li>
                       </ul>
                 </div>
                 </nav>
 
                 <div class="row">
-                    <div class="col-md-4 text-center" style="margin-top: 6em;">
-                        <h3>YOU CALL IT </h3> 
-                          <h3>MUSIC </h3> 
-                            <h3> I CALL IT MY THERAPIST </h3>
+                    <div class="col-md-6 text-center ml-5 " style="margin-top: 8em;">
+                        <h1 style="font-weight: bolder; font-size: 3em;">YOU CALL IT </h1> 
+                          <h1 style="font-weight: bolder;">MUSIC </h1> 
+                            <h3 style="font-weight: bolder; font-size: 2em;"> I CALL IT MY THERAPIST </h3>
                     </div>
-                    <div class="image col-md-6">
+                    <div class="image col-md-5">
                         <img src="../src/assets/Camera/background.png" height="500px"  />
                     </div>
                 </div>
@@ -44,9 +47,7 @@ export default {
 <style scoped>
 .body{
   font-family: Verdana, Geneva, Tahoma, sans-serif;
-  background: linear-gradient(#A10035, #FFDDDD);
-  /* background-color:linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)) #A10035; */
-  /* background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)),url(../assets/download.jfif); */
+  padding: 4em;
   background-size: cover;
   background-attachment: fixed;
   -moz-background-size: cover;
@@ -55,10 +56,11 @@ export default {
   overflow: hidden;
   height: auto;
   width: 100%;
+  background: white;
 }
 
 header{
-  background: white;
+  background: linear-gradient(#A10035, #FFDDDD);
   background-size: cover;
   background-attachment: fixed;
   -moz-background-size: cover;
@@ -69,8 +71,9 @@ header{
   width: 100%;
 }
 
-.image{
-  /* height: 2vh; */
-  /* width: 100%; */
+.image:hover{
+  transform: scale(1.1);
+  transition: all 2s;
+
 }
 </style>
