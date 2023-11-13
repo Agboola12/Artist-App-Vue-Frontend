ebed9604ca8c5c1d7fa6936ebf3b41a70f251c25
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-13 09:30:49 +0100

    working on frontend

--- src/Land.vue
+++ src/Land.vue
@@ -1,30 +1,43 @@
 <template>
         <div class="body p-5">
             <header>
-              <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Sridhar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse  navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
+              <nav class="navbar navbar-expand-lg navbar-light bg-light p-2 " style="color: #001d47" >
+                    <h4 class="navbar-brand" href="#" style="color: #A10035;">Prime</h4>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+  <div class="collapse navbar-collapse" id="navbarNav">
+    
+    <ul class="navbar-nav  mx-auto text-center" id="navbar">
+        <li class="nav-item mx-auto">
+            <a class="nav-link  text-dark text-center"  href="#" routerLink="home">Home</a>
+        </li>
+     
+     <li class="nav-item">
+        <a class="nav-link text-dark"  href="#" routerLink="/user-home">Account</a>
+    </li>
+  
+        <!-- <li class="nav-item dropdown">
+            <a class="nav-link  text-dark"     >Account</a>
+        </li> -->
+  
+      <li class="nav-item">
+        <a class="nav-link text-dark"  href="#" routerLink="/contact">Contact</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link text-dark"  href="#" routerLink="/findabranch">Find a branch</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-dark"  href="#" routerLink="/partner">Sponsorship</a>
+    </li>
+    <li class="nav-item">
+            <a class="nav-link  text-dark"  href="#" routerLink="/question">Question</a>
+        </li>
+    </ul>
+  </div>
+  </nav>
                 <nav class=" shadow navbar navbar-expand-lg navbar-light bg-light">
                     <h4 class="navbar-brand" href="#" style="color: #A10035;">Prime</h4>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
