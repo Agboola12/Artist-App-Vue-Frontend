0489820c03082a781cd3855c9a848b7835ef8ad7
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-14 15:58:56 +0100

    working on frontend

--- src/user/booking/BookingFour.vue
+++ src/user/booking/BookingFour.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container "  style="margin-top: 5em;  ">
         <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
-          <div class="col-sm-5 mx-auto " style="border: 1px solid gray; border-style: none; ">
+          <div class="col-sm-5 mx-auto " style=" ">
               <h1 class="ml-5">Booking</h1>
               <hr class="w-50 ml-4"/>
               <div class="mt-4">
