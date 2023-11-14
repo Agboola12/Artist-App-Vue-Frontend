e95af91f2b130084afb4ff34263603503605d4c9
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-14 15:25:33 +0100

    working on frontend

--- src/user/booking/BookingThree.vue
+++ src/user/booking/BookingThree.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container "  style="margin-top: 5em;  ">
     <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
-        <div class="col-sm-6 mx-auto " style="border: 1px solid gray; border-style: none; ">
+        <form class="col-sm-6 mx-auto " style="border: 1px solid gray; border-style: none; ">
             <h1 class="ml-5">Booking</h1>
             <hr class="w-50 ml-4"/>
             <div class="mt-5">
@@ -13,7 +13,7 @@
                 <button type="submit" class="btn btn text-light  w-25 m-2 ">Back</button>
                 <!-- <button class="btn btn text-light  w-25 m-2 "><a href="bookingfour">Next</a></button> -->
             </div>
-         </div> 
+         </form> 
          </div>  
 </template>
 
