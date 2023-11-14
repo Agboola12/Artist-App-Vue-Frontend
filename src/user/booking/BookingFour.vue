b97d34795b6201f3dec6c3701fee5fc84839682d
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-14 16:17:31 +0100

    working on frontend

--- src/user/booking/BookingFour.vue
+++ src/user/booking/BookingFour.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container "  style="margin-top: 5em;  ">
         <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
-          <form class="col-sm-5" style="margin-left: 25%; ">
+          <form action="post" enctype="multipart/form-data" @submit.prevent="onSend" class="col-sm-5" style="margin-left: 25%; ">
               <h1 class="ml-5">Booking</h1>
               <hr class="w-50 ml-4"/>
               <div class="mt-4">
