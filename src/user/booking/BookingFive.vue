dc19f2caa7987b23c0bbc6151761d052a74b853a
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-15 08:34:42 +0100

    working on frontend

--- src/user/booking/BookingFive.vue
+++ src/user/booking/BookingFive.vue
@@ -1,7 +1,7 @@
  <template>
         <div class="container "  style="margin-top: 5em;  ">
         <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
-          <div class="col-sm-4 mx-auto " style="border: 1px solid gray; border-style: none; ">
+          <form action="post" enctype="multipart/form-data" @submit.prevent="onSend" class="col-sm-4 mx-auto " style="border: 1px solid gray; border-style: none; ">
               <h1 class="ml-5">Booking</h1>
               <hr class="w-50 ml-5"/>
               <div class="mt-4">
@@ -10,15 +10,26 @@
               </div>
 
               <div class="mt-3 text-center">
-                  <button class="btn btn text-light  w-25 m-2 ">Back</button>
-                  <button class="btn btn text-light  w-25 m-2 "><a href="bookingsix">Next</a></button>
+                  <button class="btn btn text-light  w-50 m-2 ">Back</button>
+                  <!-- <button class="btn btn text-light  w-25 m-2 "><a href="bookingsix">Next</a></button> -->
               </div>
-           </div> 
+           </form> 
            </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
 
 }
 </script>
