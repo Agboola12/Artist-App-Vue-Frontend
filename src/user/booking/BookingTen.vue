c382b1cd31dec74eabf49ee5d8888c5eb6095d73
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-15 14:38:35 +0100

    working on frontend

--- src/user/booking/BookingTen.vue
+++ src/user/booking/BookingTen.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container "  style="margin-top: 5em;  ">
         <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
-      <div class="col-sm-6 mx-auto " style="border: 1px solid gray; border-style: none; ">
+      <div class="col-sm-6" style="margin-left: 25%; ">
           <h1 class="ml-5">Booking</h1>
           <hr class="w-50 ml-4"/>
           <div class="mt-5">
@@ -14,14 +14,10 @@
                 <button class="btn btn text-light  w-75 ">Yes, please</button>
             </div>
             <div class="col-md-6">
-                <button  id="btn" class="btn btn text-dark w-75  ">No, thanks</button>
+                <button  id="btn" class="btn btn text-dark w-75 ">No, thanks</button>
             </div>
           </div>
 
-          <!-- <div class="mt-5 ml-3">
-              <button class="btn btn text-light  w-25 m-1 ">Back</button>
-              <button class="btn btn text-light  w-25 m-1 "><a href="booking">Next</a></button>
-          </div> -->
        </div> 
        </div>
 </template>
