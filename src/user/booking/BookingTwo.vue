08f8607905bdf85ee63ef56b9aba01ce5dabf810
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-15 11:29:44 +0100

    working on frontend

--- src/user/booking/BookingTwo.vue
+++ src/user/booking/BookingTwo.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container "  style="margin-top: 5em;  ">
         <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
-        <form action="post" enctype="multipart/form-data" @submit.prevent="onCreate(user)" class="col-sm-4" style="margin-left: 30%;" >
+        <form action="post" enctype="multipart/form-data" @submit.prevent="onCreate()" class="col-sm-4" style="margin-left: 30%;" >
             <h1 class="ml-5">Booking</h1>
             <hr class="w-50 ml-4"/>
             <div class="mt-5">
@@ -44,17 +44,17 @@ export default {
 
     },
     methods:{
-        onCreate(user){
-                const info={
-                    artistType:this.artistType,
-                    musicType:this.musicType
-                }
+        onCreate(){
+                // const info={
+                //     artistType:this.artistType,
+                //     musicType:this.musicType
+                // }
 
-                useCounterStore.users
+                // useCounterStore.users
 
-        //     const user = localStorage.setItem('users', JSON.stringify({artistType:this.artistType,musicType:this.musicType}))
-        //    console.log(user); 
-        //    this.$router.push('/bookingthree')
+            const user = localStorage.setItem('users', JSON.stringify({artistType:this.artistType,musicType:this.musicType}))
+           console.log(user); 
+           this.$router.push('/bookingthree')
 
         }
 
