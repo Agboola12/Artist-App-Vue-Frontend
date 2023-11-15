be506bd8203e2bf4c6db996a3940ce53841abb29
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-15 09:59:51 +0100

    working on frontend

--- src/user/booking/BookingFive.vue
+++ src/user/booking/BookingFive.vue
@@ -1,6 +1,6 @@
  <template>
         <div class="container mx-auto"  style="margin-top: 5em;  ">
-        <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
+        <a href="homeuser" style="margin-left: 90%;"><button class="btn btn p-3">Go Back Home</button></a>
           <form action="post" enctype="multipart/form-data" @submit.prevent="onSend" class="col-sm-4" style="margin-left: 30%;">
               <h1 class="ml-5">Booking</h1>
               <hr class="w-50 ml-5"/>
@@ -9,8 +9,9 @@
                      <input type="search" class="form-control" v-model="time" placeholder="2 : 00 pm"/>
               </div>
 
-              <div class="mt-3 text-center">
-                  <button class="btn btn text-light  w-50 m-2 ">Back</button>
+              <div class="mt-5 text-center">
+                <button type="submit" class="button p-3 w-50"> Next</button>
+                  <!-- <button type="submit" class="btn btn text-light  w-50 m-2 ">Next</button> -->
                   <!-- <button class="btn btn text-light  w-25 m-2 "><a href="bookingsix">Next</a></button> -->
               </div>
            </form> 
@@ -30,7 +31,9 @@ export default {
     methods:{
 
         onSend(){
-            
+            let getUsers = localStorage.users?JSON.parse(localStorage.getItem ("users")):{};
+            localStorage.setItem('users', JSON.stringify({...getUsers,time:this.time, }))
+           this.$router.push('/bookingsix')
         }
 
     }
@@ -40,8 +43,49 @@ export default {
 
 <style scoped>
 
-    button{
+    .btn{
+          color: white;
           background-color: #A10035;
       }
+
+      .button {
+  /* padding: ; */
+  border: none;
+  border-radius: 5px;
+  /* font-weight: bold; */
+  letter-spacing: 5px;
+  text-transform: uppercase;
+  color: #A10035;
+  transition: all 1000ms;
+  font-size: 15px;
+  position: relative;
+  overflow: hidden;
+  outline: 2px solid #A10035;
+}
+
+.button:hover {
+  color: white;
+  transform: scale(1.1);
+  outline: 2px solid #A10035;
+  box-shadow: 4px 5px 17px -4px #A10035;
+}
+
+.button::before {
+  content: "";
+  position: absolute;
+  left: -50px;
+  top: 0;
+  width: 0;
+  height: 100%;
+  background-color: #A10035;
+  transform: skewX(45deg);
+  z-index: -1;
+  transition: width 1000ms;
+}
+
+.button:hover::before {
+  width: 250%;
+}
+
 
 </style>
