7be4bcec5afd528373d5eb7776f843f961224cd6
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-18 00:28:41 +0100

    working on validation

--- src/artist/RegisterArtist.vue
+++ src/artist/RegisterArtist.vue
@@ -147,7 +147,7 @@ import FooterArtist from "./FooterArtist.vue";
       validateForm() {
       this.errors = {};
 
-      if (!this.firstName && this.firstName > 0 || this.firstName < 4) {
+      if (!this.firstName && this.firstName < 0 || this.firstName > 4) {
         this.errors.name = 'Name is required and the name should be more than four letters word';
       }
       if (!this.email) {
