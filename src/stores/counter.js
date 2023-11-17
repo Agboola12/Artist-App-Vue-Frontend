800fb18ebca5651346bb1a57b726465bb69c941e
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-17 11:30:16 +0100

    working on frontend

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -14,11 +14,7 @@
   
   
   // store.js
-import { createPinia } from 'pinia';
 
-const pinia = createPinia();
-
-export const useMyStore = pinia(() => {
   const state = {
     artistType: '',
     musicType: '',
