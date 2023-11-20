2de8eac522e4cb99c7b56221968e34aed26d57ba
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-20 10:59:04 +0100

    working on frontend

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,12 +4,14 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore('counter', () => {
   
   const users= ref([])
+  const user= ref([])
 
   function bookingOne(user) {
     users.push(user)
     }
+    
   function bookingTwo(user) {
-    users.push(user)
+    user.push(user)
     }
 
   return {  bookingOne,bookingTwo,users }
