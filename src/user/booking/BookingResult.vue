3a07290847c253a6e0eeed7cee9c86eca436765c
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-22 14:06:36 +0100

    working on the booking information at the frontend

--- src/user/booking/BookingResult.vue
+++ src/user/booking/BookingResult.vue
@@ -1,23 +1,20 @@
 <template>
     <div class="container "  style="margin-top: 5em;  ">
         <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
-      <div class="col-sm-6" style="margin-left: 25%; ">
-          <h1 class="ml-5">Booking</h1>
-          <hr class="w-50 ml-4"/>
-          <div class="mt-5">
-              <h4>Would you like to see musicians similar to your taste?</h4>
-              <p> Your inquiry will also be sent to similar musicians. </p>
-          </div>
 
-          <div class="row mt-5">
-            <div class="col-md-6">
-                <button class="btn btn text-light  w-75 " @click="onSend()">Yes, please</button>
+        <div class="container mt-4 mb-5" style="width: auto;">
+          <div class='d-flex '>
+            <h4>Top Songs</h4>
+            <p id="ppa" style="margin-left: 70%;"> <a href="allsong">View All</a></p>
+        </div>
+            <div class=' shadow-sm p-3 mb-5 d-flex m-2' v-for="(song, index) in songs" :key="index" id="song">
+              <p>0{{ index + 1 }}.</p>
+              <img :src="song.imageUrl" alt="cover" class="ml-4" width="50" height="50" style="border-radius: 50px;"/> 
+              <p class="ml-4 mr-4"> {{ song.songTitle }}</p>
+              <audio  controls> <source :src="song.mp3Url" type="audio/mpeg" ></audio>
             </div>
-            <div class="col-md-6">
-                <button  id="btn" class="btn btn text-dark w-75 ">No, thanks</button>
-            </div>
-          </div>
-       </div> 
+        </div>
+
        </div>
 </template>
 
@@ -29,6 +26,7 @@ export default {
         return{
             // userName: '',
             // userEmail: '',
+            user: null
         }
     },
     created(){
@@ -55,7 +53,7 @@ export default {
             //   userEmail:this.userEmail
             //    "http://localhost:8000/bookingInfo", {...bookingInfo, ...loginUser})
             // }
-            
+
             const bookingInfo = JSON.parse(localStorage.getItem('users'));
             console.log(bookingInfo);
               
@@ -64,6 +62,7 @@ export default {
                    "http://localhost:8000/getBookingInfo",bookingInfo)
                  .then((res) => {
                    console.log(res.data.user);
+                   this.user = res.data.user
                  })
                  .catch((err) => {
                    console.log(err);
