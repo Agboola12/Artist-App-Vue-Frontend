63ab3ad26b330ae48e11b435a1bdd6e5bf4ca4ba
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-23 11:38:11 +0100

    working on the booking information at the frontend

--- src/user/booking/ArtistProfile.vue
+++ src/user/booking/ArtistProfile.vue
@@ -201,7 +201,11 @@ export default {
     }
 
     .hire{
-        
+        border: 2px solid #A10035;
+        color: #A10035;
+        justify-content: center;
+        justify-self: center;
+        justify-items: center;
     }
 
 </style>
