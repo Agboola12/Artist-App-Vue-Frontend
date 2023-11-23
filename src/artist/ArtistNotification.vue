6b3ce53e90794e463ca3deebc500d2ed957ad038
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-23 14:35:22 +0100

    working on the booking information at the frontend

---
+++ src/artist/ArtistNotification.vue
@@ -0,0 +1,30 @@
+<template>
+  
+</template>
+
+<script>
+import axios from "axios";
+
+
+export default {
+  data() {
+    return {
+
+
+    };
+  },
+  created(){
+
+  },
+
+  methods: {
+    
+        artistNotice(){
+            axios.get()
+
+        }
+    
+
+  }
+}
+</script>
