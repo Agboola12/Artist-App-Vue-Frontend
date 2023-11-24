969f364f9d2e7925ec5058f967324e22e03f533b
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-24 12:29:25 +0100

    working on frontend

--- src/artist/ArtistNotification.vue
+++ src/artist/ArtistNotification.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="body">
         <NavArtist/>
-        <div class="container">
-            <div class="row">
-                    <div class="col-md-3 card-client" v-for="(user, index) in users" :key="index">
+        <div class="container ">
+            <div class="row " >
+                    <div class="col-md-3 m-5 card-client" v-for="(user, index) in users" :key="index" >
                 <div class="user-picture">
                     <svg viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
                         <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"></path>
                     </svg>
                 </div>
-                <p class="name-client"> {{ user.userName }}
+                <p class="name-client"> 
+                    {{ user.userName }}
                     <span>{{ user.userEmail }}</span>
+                    <span><a href="" class="details">Details</a></span>
                 </p>
                 </div>
             </div>
@@ -46,6 +48,7 @@ export default {
             axios.get(`http://localhost:8000/artistNotice/${this.id}`)
                 .then((res) => {
                 console.log(res.data.user);
+                this.users = res.data.user
             })
                 .catch((error) => {
                 console.error(error);
@@ -69,7 +72,7 @@ export default {
 }
 
 .card-client {
-  margin-left: 10em;  
+  /* margin-left: 10em;   */
   background: #A10035;
   width: 20rem;
   height: 15em;
@@ -88,6 +91,10 @@ export default {
 
 .card-client:hover {
   transform: translateY(-10px);
+}
+
+.details{
+    color: #fddbdb;
 }
 
 .user-picture {
