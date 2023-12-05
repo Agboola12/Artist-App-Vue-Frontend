296d110e288968145ba1350f48936f216924930f
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-05 12:48:11 +0100

    adjusting some design

--- src/artist/NavArtist.vue
+++ src/artist/NavArtist.vue
@@ -1,32 +1,50 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
+    <nav class="navbar navbar-expand-sm navbar-light bg-dark fixed-top " id="home" >
+<a href="" class="navbar-brand p-3"><img src="images/logo.jpg" style="width:60px;"></a>
+<button class="navbar-toggler" data-toggle="collapse" type="button" data-target="#sab">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="sab">    
+<ul class="navbar-nav list-line ml-auto">               
+    <li class="nav-item active list-line-item mr-5 ">
+      <a href="#home" class="nav-link active text-center text-light">Home</a>
+    </li>
+
+    <li class="nav-item dropdown list-line-item mr-5 " >
+      <a href="#gallery" class="nav-link text-center text-light">Gallery</a>
+    </li>
+    <li class="nav-item dropdown  list-line-item mr-5" >
+    <a href="#about" class="nav-link text-center text-light" >About</a>
+    </li>
+    <li class="nav-item dropdown  list-line-item mr-5">
+      <a href="#contact" class="nav-link text-center text-light">Contact</a>
+    </li>
+    <li class="nav-item dropdown  list-line-item mr-5">
+      <a href="form.php" class="nav-link text-center text-light">Order</a>
+    </li>
+    <li class="nav-item dropdown  list-line-item mr-5">
+      <a href="Worker/login.php" class="nav-link text-center text-light">Workers</a>
+    </li>
+    <li class="nav-item dropdown  list-line-item mr-5">
+      <a href="Admin/adminlogin.php" class="nav-link text-center text-light">Admin</a>
+    </li>
+  </ul>
+
+</div>
 </nav>
+
+
+
+
     <nav class="navbar navbar-expand-lg navbar-light border-bottom" style="background-color: white">
       <a class="navbar-brand" id="aa" style="color: #a10035" href="#">Prime</a>        
       <button  class="navbar-toggler"  type="button"  data-toggle="collapse"  data-target="#navbarNav"  aria-controls="navbarNav"  aria-expanded="false"  aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <button class="navbar-toggler" data-toggle="collapse" type="button" data-target="#sab">
+  <span class="navbar-toggler-icon"></span>
+</button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav fs-4" style="margin-left: 30%; ">
           <li class="nav-item active">
