715417ab1b6ea736c4b438f4c430b211e5efb445
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-12 11:08:43 +0100

    adjusting the designs

--- src/Land.vue
+++ src/Land.vue
@@ -86,26 +86,26 @@
                     </div>
                  </div> 
                  
-                 <div class="row mt-4">
-                    <div class="col-md-4 " style="margin-top: 11em; text-align: center;" >
+                 <div class="d-flex" style="margin-top: 7em;">
+                    <div class="text " style=" text-align: center;" >
                         <h2>
                             “Band may be hard work, but it's worth it when we see the crowd on their feet.”
                         </h2>
                     </div>
-                    <div class="col-md-6 text-center">
-                      <img src="../src/assets/Camera/band.png" class="w-50"  />
+                    <div class="mt-3 text-center">
+                      <img src="../src/assets/Camera/band.png" class="w-75"  />
                     </div>
                   </div> 
                 
                 <div class=" mt-5 p-5">
                   <div class="row">
-                    <div class="col-md-4 offset-1" style="margin-top: 6 em; text-align: center;" >
+                    <div class="col-md-4 " style="margin-top: 6 em; text-align: center;" >
                       <h2>
                         I think music in itself is healing. 
                       </h2>
                     </div>
-                    <div class="col-md-5 offset-2">
-                      <img src="../src/assets/Camera/dj.jpg" height="30  />
+                    <div class="col-md-5">
+                      <img src="../src/assets/Camera/dj.jpg" class="w-50"  />
                     </div>
                   </div> 
                 </div>
@@ -213,5 +213,17 @@ header{
 
 .single{
   background: #FFDDDD;
+}
+
+@media only screen and (max-width: 1200px){
+    /*Tablets [601px -> 1200px]*/
+}
+@media only screen and (max-width: 600px){
+	/*Big smartphones [426px -> 600px]*/
+}
+@media only screen and (max-width: 425px){
+    .text{
+      margin-top: 4r;
+    }
 }
 </style>
