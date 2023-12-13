8d2cc6214e768c086420560032b3d65813a7bb2e
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-13 10:46:44 +0100

    adjustment in the project

--- src/user/booking/BookingTwo.vue
+++ src/user/booking/BookingTwo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container "  style="margin-top: 5em;  ">
-        <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a>
+        <!-- <a href="homeuser" style="margin-left: 90%;"><button class="btn btn-danger  ">Go Back Home</button></a> -->
         <form action="post" enctype="multipart/form-data" @submit.prevent="onCreate()" class="col-sm-4" style="margin-left: 30%;" >
             <h1 class="ml-5">Booking</h1>
             <hr class="w-50 ml-4"/>
